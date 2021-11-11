a1a8f7c913ab55df023b548cb781ac8693df60ac
Walee Sebiiy <44040669+nyuuke@users.noreply.github.com>
2021-11-11 10:01:01 +0100

    Create a declarations file for typescript

--- vercel/components/hamburger/MenuIcon.tsx
+++ vercel/components/hamburger/MenuIcon.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-const MenuIcon = () => {
+const MenuIcon: FC = () => {
   return <></>
 }
 
--- vercel/theme/theme.ts
+++ vercel/theme/theme.ts
@@ -1,38 +1,6 @@
 import 'styled-components'
 import { DefaultTheme } from 'styled-components'
 
-declare module 'styled-components' {
-  interface DefaultTheme {
-    scheme: {
-      body: string
-      text: string
-      domi: string
-    }
-    accent: {
-      white: string
-      hover: string
-      select: string
-      disabled: string
-      textshade: string
-      hoverSahdeForm: string
-    }
-
-    units: {
-      _14: string
-      _16: string
-      _18: string
-      _24: string
-      _30: string
-      _36: string
-      _42: string
-      _48: string
-      _60: string
-      _72: string
-      _84: string
-    }
-  }
-}
-
 export const lightTheme: DefaultTheme = {
   scheme: {
     body: '#fcfbfc',
