8acb1739fcccc4aa3e3b98380f04e6bf9263a388
Walee Sebiiy <44040669+nyuuke@users.noreply.github.com>
2022-01-22 19:03:32 +0100

    Update useAxios.tsx

--- client/hooks/useAxios.tsx
+++ client/hooks/useAxios.tsx
@@ -3,4 +3,5 @@ import axios, { AxiosInstance } from 'axios'
 export const __fetch: AxiosInstance = axios.create({
   withCredentials: true,
   baseURL: 'https://koulwakel.herokuapp.com',
+  // baseURL: 'http://localhost:8000',
 })
